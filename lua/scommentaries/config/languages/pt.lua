a2b8251923566d6789b198714ba0dfa7b9feda06
BrownJourney <lapshabeast@trigon.im>
2021-03-03 17:55:47 +0300

    Added Portuguese language

---
+++ lua/scommentaries/config/languages/pt.lua
@@ -0,0 +1,55 @@
+local lang = {}
+lang['category_cats'] = 'Categorias'
+lang['category_create'] = 'Adicionar comentário'
+lang['category_control'] = 'Meus comentários'
+lang['category_logs'] = 'Registro de comentários'
+
+lang['text_self'] = 'TEXTO'
+lang['text_type'] = 'TIPO'
+lang['text_unknown'] = 'desconhecido'
+lang['text_removing'] = 'Será removido em %s'
+lang['text_submit'] = 'Confirmar'
+lang['text_creator'] = 'Criador'
+lang['text_anon'] = 'Anônimo'
+lang['text_datecreation'] = 'Data de criação'
+lang['text_timetoremove'] = 'Tempo restante'
+lang['text_comm'] = 'Texto'
+lang['text_remove'] = 'Remover comentário'
+lang['text_cancel'] = 'Cancelar'
+lang['text_moveto'] = 'Teleportar a comentário'
+lang['text_copysid'] = 'Copiar SteamID'
+lang['text_days'] = 'dias'
+lang['text_hours'] = 'horas'
+lang['text_minutes'] = 'minutos'
+lang['text_infinite'] = 'infinito'
+lang['text_yes'] = 'Sim'
+lang['text_no'] = 'Não'
+lang['text_duration'] = 'DURAÇÃO'
+lang['text_permanent'] = 'Tornar permanente'
+lang['text_buttonlike'] = 'Aperte [%s] para dar like neste comentário'
+lang['text_likes'] = 'Likes'
+
+lang['check_anon'] = 'Permanecer anônimo'
+lang['check_perm'] = 'Comentário permanente'
+
+lang['notify_changedtype'] = 'Você mudou o tipo de comentário com sucesso!'
+lang['notify_fieldempty'] = "Você não pode deixar o campo de texto em branco!"
+lang['notify_niltype'] = "Você não especificou o tipo de comentário"
+lang['notify_copysid'] = "Você copiou o SteamID do autor com sucesso"
+lang['notify_serverlimit'] = 'Limite do servidor de comentários foi alcançado!'
+lang['notify_accountlimit'] = 'Você alcançou o limite máximo de comentários na sua conta!'
+lang['notify_nearcomm'] = 'Há um comentário por perto!'
+lang['notify_ground'] = 'Você só pode deixar comentários no chão!'
+lang['notify_moveto'] = 'Você teleportou ao comentário #%s'
+lang['notify_remove'] = 'Você removeu o comentário#%s'
+lang['notify_undefined'] = "O comentário #%s não existe mais!"
+lang['notify_cantafford'] = "Você não pode fazer um comentário!"
+lang['notify_commcreation'] = 'Você fez um comentário com sucesso!'
+lang['notify_fieldsmall'] = 'Seu texto é muito curto!'
+lang['notify_fieldlarge'] = 'Seu texto é muito longo!'
+lang['notify_permanent'] = 'Você mudou a duração do comentário #%s para permanente!'
+lang['notify_errorlike'] = 'Você já deu like nesse comentário!'
+lang['notify_likecomm'] = 'Você deu like nesse comentário!'
+lang['notify_likelimit'] = 'Esse comentário alcançou o limite de likes!'
+
+comms.language = lang
