911908717de38f01c811b484d67d501575885b0a
BrownJourney <lapshabeast@trigon.im>
2021-03-20 19:54:27 +0300

    Added Turkish language

---
+++ lua/scommentaries/config/languages/tr.lua
@@ -0,0 +1,55 @@
+local lang = {}
+lang['category_cats'] = 'Kategoriler'
+lang['category_create'] = 'Yorum ekle'
+lang['category_control'] = 'Yorumlarım'
+lang['category_logs'] = 'Yorum Kayıtları'
+
+lang['text_self'] = 'METİN'
+lang['text_type'] = 'Tip'
+lang['text_unknown'] = 'bilinmeyen'
+lang['text_removing'] = 'Silinecek %s'
+lang['text_submit'] = 'Onayla'
+lang['text_creator'] = 'Yapan'
+lang['text_anon'] = 'Anonim'
+lang['text_datecreation'] = 'Yapım Tarihi'
+lang['text_timetoremove'] = 'Kalan Zaman'
+lang['text_comm'] = 'Metin'
+lang['text_remove'] = 'Yorumu Sil'
+lang['text_cancel'] = 'İptal'
+lang['text_moveto'] = 'Yoruma Işınlan'
+lang['text_copysid'] = 'SteamID Kopyala'
+lang['text_days'] = 'gün'
+lang['text_hours'] = 'saat'
+lang['text_minutes'] = 'dakika'
+lang['text_infinite'] = 'sınırsız'
+lang['text_yes'] = 'Evet'
+lang['text_no'] = 'Hayır'
+lang['text_duration'] = 'Süre'
+lang['text_permanent'] = 'Kalıcı Yap'
+lang['text_buttonlike'] = "Yorumu beğenmek için [%s] 'e bas"
+lang['text_likes'] = 'Beğeniler'
+
+lang['check_anon'] = 'Anonim Kal'
+lang['check_perm'] = 'Kalıcı Yorum'
+
+lang['notify_changedtype'] = 'Başarıyla yorum tipini değiştirdin!'
+lang['notify_fieldempty'] = "Bu metini boş bırakamazsın!"
+lang['notify_niltype'] = "Yorum tipi belirtmedin"
+lang['notify_copysid'] = "Başarıyla sahibin SteamID'sini kopyaladın"
+lang['notify_serverlimit'] = 'Server maksimum yorum limitine ulaştı!'
+lang['notify_accountlimit'] = 'Hesabın maksimum yorum limitine ulaştı!!'
+lang['notify_nearcomm'] = 'Yakınlarda bir yorum var!'
+lang['notify_ground'] = 'Yorumları sadece zemine yapabilirsin!'
+lang['notify_moveto'] = '#%s Yorumuna ışınlandın'
+lang['notify_remove'] = '#%s Yorumunu sildin'
+lang['notify_undefined'] = "Yorum #%s artık yok"
+lang['notify_cantafford'] = "Yorum yapmaya paran yok"
+lang['notify_commcreation'] = 'Başarıyla yorum yaptın!'
+lang['notify_fieldsmall'] = 'Metinin çok kısa!'
+lang['notify_fieldlarge'] = 'Metinin çok uzun!'
+lang['notify_permanent'] = '#%s Başarıyla yorumun süresini kalıcı yaptın!'
+lang['notify_errorlike'] = 'Bu yorumu zaten beğendin!'
+lang['notify_likecomm'] = 'Bu yorumu beğendin!'
+lang['notify_likelimit'] = 'Bu yorum beğeni limitine ulaştı!'
+
+comms.language = lang
